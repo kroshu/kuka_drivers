0d177e558b5ae7cd43ab52a7171d42b3e6385a06
Zoltan Resi <resizoltan@gmail.com>
2019-11-07 12:37:25 +0100

    all publishers added

--- include/kuka_sunrise_interface/robot_observer.hpp
+++ include/kuka_sunrise_interface/robot_observer.hpp
@@ -10,15 +10,74 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/int64.hpp"
+#include "std_msgs/msg/float64.hpp"
+
+#include "fri_client/friLBRClient.h"
+#include <list>
 
 namespace kuka_sunrise_interface{
 
-class RobotObserver{
+//TODO: use message types with headers instead?
+class InputPublisherBase{
+public:
+  InputPublisherBase(std::string name, const KUKA::FRI::LBRState& robot_state, rclcpp::Node::SharedPtr robot_control_node);
+  virtual void publishInputValue(const KUKA::FRI::LBRState& robot_state) = 0;
+  virtual ~InputPublisherBase();
+protected:
+  virtual void createPublisher() = 0;
+  std::string name_;
+  const KUKA::FRI::LBRState& robot_state_;
+  rclcpp::Node::SharedPtr robot_control_node_;
+};
 
+class BooleanInputPublisher: public InputPublisherBase{
+public:
+  using InputPublisherBase::InputPublisherBase;
+  virtual void publishInputValue();
+  virtual void createPublisher();
+  //virtual ~BooleanInputPublisher();
+private:
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr publisher_;
+};
+
+class DigitalInputPublisher: public InputPublisherBase{
+public:
+  using InputPublisherBase::InputPublisherBase;
+  virtual void publishInputValue();
+  virtual void createPublisher();
+  //virtual ~DigitalInputPublisher();
+private:
+  rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr publisher_;
+};
+
+class AnalogInputPublisher: public InputPublisherBase{
+public:
+  using InputPublisherBase::InputPublisherBase;
+  virtual void publishInputValue();
+  virtual void createPublisher();
+  //virtual ~AnalogInputPublisher();
+private:
+  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_;
+};
+
+class RobotObserver{
+  RobotObserver(const KUKA::FRI::LBRState& robot_state, rclcpp::Node::SharedPtr robot_control_node);
+  void publishRobotState();
 
 private:
-  rclcpp::Publisher<sensor_msgs::msg::JointState> joint_state_publisher_;
+  const KUKA::FRI::LBRState& robot_state_;
+  sensor_msgs::msg::JointState joint_state_msg_;
+
+  rclcpp::Node::SharedPtr robot_control_node_;
+  rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher_;
+  std::list<std::unique_ptr<InputPublisherBase>> input_publishers_;
+
+  rclcpp::Clock ros_clock_;
 };
+
+
 
 
 }
--- src/robot_observer.cpp
+++ src/robot_observer.cpp
@@ -5,8 +5,85 @@
  *      Author: rosdeveloper
  */
 
+#include "kuka_sunrise_interface/robot_observer.hpp"
+
+#include "rclcpp/time.hpp"
 
 namespace kuka_sunrise_interface{
+
+InputPublisherBase::InputPublisherBase(std::string name, const KUKA::FRI::LBRState& robot_state, rclcpp::Node::SharedPtr robot_control_node):
+    name_(name),
+    robot_state_(robot_state),
+    robot_control_node_(robot_control_node)
+{
+}
+
+void BooleanInputPublisher::createPublisher(){
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(1));
+  qos.best_effort();
+  publisher_ = robot_control_node_->create_publisher<std_msgs::msg::Bool>(name_, qos);
+}
+
+void BooleanInputPublisher::publishInputValue(){
+  std_msgs::msg::Bool msg;
+  msg.data = robot_state_.getBooleanIOValue(name_.c_str()); //TODO: catch exception
+  publisher_->publish(msg);
+}
+
+void DigitalInputPublisher::createPublisher(){
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(1));
+  qos.best_effort();
+  publisher_ = robot_control_node_->create_publisher<std_msgs::msg::Int64>(name_, qos);
+}
+
+void DigitalInputPublisher::publishInputValue(){
+  std_msgs::msg::Int64 msg;
+  msg.data = robot_state_.getDigitalIOValue(name_.c_str()); //TODO: catch exception
+  publisher_->publish(msg);
+}
+
+void AnalogInputPublisher::createPublisher(){
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(1));
+  qos.best_effort();
+  publisher_ = robot_control_node_->create_publisher<std_msgs::msg::Float64>(name_, qos);
+}
+
+void AnalogInputPublisher::publishInputValue(){
+  std_msgs::msg::Float64 msg;
+  msg.data = robot_state_.getAnalogIOValue(name_.c_str()); //TODO: catch exception
+  publisher_->publish(msg);
+}
+
+RobotObserver::RobotObserver(const KUKA::FRI::LBRState& robot_state, rclcpp::Node::SharedPtr robot_control_node):
+    robot_state_(robot_state),
+    robot_control_node_(robot_control_node),
+    ros_clock_(RCL_ROS_TIME)
+{
+  joint_state_msg_.position.reserve(robot_state_.NUMBER_OF_JOINTS);
+  joint_state_msg_.velocity.reserve(robot_state_.NUMBER_OF_JOINTS);
+  joint_state_msg_.effort.reserve(robot_state_.NUMBER_OF_JOINTS);
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(1));
+  qos.best_effort();
+  robot_control_node->create_publisher<sensor_msgs::msg::JointState>("lbr_joint_state", qos);
+}
+
+void RobotObserver::publishRobotState(){
+  const double* joint_positions = robot_state_.getMeasuredJointPosition();
+  const double* joint_torques = robot_state_.getExternalTorque(); //TODO: external vs measured?
+
+  joint_state_msg_.header.frame_id = "world";
+  joint_state_msg_.header.stamp = ros_clock_.now();//TODO catch exceptions
+
+  joint_state_msg_.velocity.clear();
+  joint_state_msg_.position.assign(joint_positions, joint_positions + robot_state_.NUMBER_OF_JOINTS);
+  joint_state_msg_.effort.assign(joint_positions, joint_torques + robot_state_.NUMBER_OF_JOINTS);
+  joint_state_publisher_->publish(joint_state_msg_);
+  //TODO double check this:
+  for(auto i = std::next(input_publishers_.begin()); i != input_publishers_.end(); i++){
+    (*i)->publishInputValue(robot_state_);
+  }
+}
+
 
 
 
