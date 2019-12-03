fb6394a076b9b12e53dbde2586929cfcd34c36ec
Zoltan Resi <resizoltan@gmail.com>
2019-12-03 15:52:20 +0100

    launch description

---
+++ launch/kuka_sunrise.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'node_prefix',
+            default_value=[''],
+            description='prefix for node names'),
+        launch_ros.actions.LifecycleNode(
+            package='kuka_sunrise', node_executable='robot_manager_node', output='screen',
+            node_name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'robot_manager']),
+        launch_ros.actions.LifecycleNode(
+            package='kuka_sunrise', node_executable='robot_control_node', output='screen',
+            node_name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'robot_control'])
+        ])
