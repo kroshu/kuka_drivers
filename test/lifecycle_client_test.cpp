911feff7fead52645fa1d31c3538eabff00dda60
Aron Svastits <svastits.aron@gmail.com>
2022-02-08 08:54:59 +0100

    uncrustify

--- test/lifecycle_client_test.cpp
+++ test/lifecycle_client_test.cpp
@@ -144,7 +144,7 @@ int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::executors::MultiThreadedExecutor executor(
-      rclcpp::ExecutorOptions(), 2);
+    rclcpp::ExecutorOptions(), 2);
   auto node = std::make_shared<LifecycleClientTester>();
   executor.add_node(node->get_node_base_interface());
   executor.spin();
