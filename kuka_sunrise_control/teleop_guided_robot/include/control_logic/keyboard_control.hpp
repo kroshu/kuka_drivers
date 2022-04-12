5bfee69a6a9ba1283d2ce8d89181459b2d88c3b3
Gergely Kovacs <kovacsge.work@gmail.com>
2022-04-12 11:26:35 +0200

    fix linter issues, replace deprecated, remove internal folder

--- kuka_sunrise_control/teleop_guided_robot/include/control_logic/keyboard_control.hpp
+++ kuka_sunrise_control/teleop_guided_robot/include/control_logic/keyboard_control.hpp
@@ -1,12 +1,23 @@
-/*
- * keyboard_control.hpp
- *
- *  Created on: Dec 2, 2019
- *      Author: rosdeveloper
- */
+// Copyright 2020 Zoltán Rési
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef INCLUDE_CONTROL_LOGIC_KEYBOARD_CONTROL_HPP_
-#define INCLUDE_CONTROL_LOGIC_KEYBOARD_CONTROL_HPP_
+#ifndef CONTROL_LOGIC__KEYBOARD_CONTROL_HPP_
+#define CONTROL_LOGIC__KEYBOARD_CONTROL_HPP_
+
+#include <string>
+#include <vector>
+#include <map>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
@@ -14,7 +25,8 @@
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
-namespace teleop_guided_robot{
+namespace teleop_guided_robot
+{
 
 struct ParameterSetAccessRights
 {
@@ -24,8 +36,7 @@ struct ParameterSetAccessRights
   bool finalized;
   bool isSetAllowed(std::uint8_t current_state) const
   {
-    switch (current_state)
-    {
+    switch (current_state) {
       case lifecycle_msgs::msg::State::PRIMARY_STATE_UNCONFIGURED:
         return unconfigured;
       case lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE:
@@ -43,36 +54,39 @@ struct ParameterSetAccessRights
 class KeyboardControl : public rclcpp_lifecycle::LifecycleNode
 {
 public:
-  KeyboardControl(const std::string& node_name, const rclcpp::NodeOptions& options);
+  KeyboardControl(const std::string & node_name, const rclcpp::NodeOptions & options);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_configure(const rclcpp_lifecycle::State&);
+  on_configure(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_cleanup(const rclcpp_lifecycle::State&);
+  on_cleanup(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_shutdown(const rclcpp_lifecycle::State&);
+  on_shutdown(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_activate(const rclcpp_lifecycle::State&);
+  on_activate(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_deactivate(const rclcpp_lifecycle::State&);
+  on_deactivate(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_error(const rclcpp_lifecycle::State&);
-
+  on_error(const rclcpp_lifecycle::State &);
 
 private:
-  rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::JointState>::SharedPtr reference_joint_state_publisher_;
+  rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::JointState>::SharedPtr
+    reference_joint_state_publisher_;
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr key_teleop_subscription_;
   void messageReceivedCallback(geometry_msgs::msg::Twist::SharedPtr msg);
 
-  rcl_interfaces::msg::SetParametersResult onParamChange(const std::vector<rclcpp::Parameter> &parameters);
-  bool canSetParameter(const rclcpp::Parameter &param);
-  bool onLowerLimitsChangeRequest(const rclcpp::Parameter& param);
-  bool onUpperLimitsChangeRequest(const rclcpp::Parameter& param);
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_callback_;
+
+  rcl_interfaces::msg::SetParametersResult onParamChange(
+    const std::vector<rclcpp::Parameter> & parameters);
+  bool canSetParameter(const rclcpp::Parameter & param);
+  bool onLowerLimitsChangeRequest(const rclcpp::Parameter & param);
+  bool onUpperLimitsChangeRequest(const rclcpp::Parameter & param);
   std::map<std::string, struct ParameterSetAccessRights> parameter_set_access_rights_;
   std::vector<double> lower_limits_rad_;
   std::vector<double> upper_limits_rad_;
@@ -80,7 +94,8 @@ private:
   int active_joint_;
   bool changing_joint_;
 
-  const struct{
+  const struct
+  {
     double X_POS = 1.25;
     double X_NEG = 2;
     double Z = 1;
@@ -91,16 +106,13 @@ private:
   rclcpp::Time last_time_;
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn SUCCESS =
-      rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn ERROR =
-      rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn FAILURE =
-      rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::FAILURE;
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::FAILURE;
 };
 
+}  // namespace teleop_guided_robot
 
-}
-
-
-
-#endif /* INCLUDE_CONTROL_LOGIC_KEYBOARD_CONTROL_HPP_ */
+#endif  // CONTROL_LOGIC__KEYBOARD_CONTROL_HPP_
--- kuka_sunrise_control/teleop_guided_robot/include/control_system/system_manager.hpp
+++ kuka_sunrise_control/teleop_guided_robot/include/control_system/system_manager.hpp
@@ -1,12 +1,23 @@
-/*
- * system_manager.hpp
- *
- *  Created on: Dec 3, 2019
- *      Author: rosdeveloper
- */
+// Copyright 2020 Zoltán Rési
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#ifndef INCLUDE_CONTROL_SYSTEM_KEYBOARD_CONTROL_HPP_
-#define INCLUDE_CONTROL_SYSTEM_KEYBOARD_CONTROL_HPP_
+
+#ifndef CONTROL_SYSTEM__SYSTEM_MANAGER_HPP_
+#define CONTROL_SYSTEM__SYSTEM_MANAGER_HPP_
+
+#include <string>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
@@ -21,35 +32,35 @@ namespace teleop_guided_robot
 class SystemManager : public rclcpp_lifecycle::LifecycleNode
 {
 public:
-  SystemManager(const std::string& node_name, const rclcpp::NodeOptions& options);
+  SystemManager(const std::string & node_name, const rclcpp::NodeOptions & options);
 
   void activateControl();
   void deactivateControl();
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_configure(const rclcpp_lifecycle::State&);
+  on_configure(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_cleanup(const rclcpp_lifecycle::State&);
+  on_cleanup(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_shutdown(const rclcpp_lifecycle::State&);
+  on_shutdown(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_activate(const rclcpp_lifecycle::State&);
+  on_activate(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_deactivate(const rclcpp_lifecycle::State&);
+  on_deactivate(const rclcpp_lifecycle::State &);
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_error(const rclcpp_lifecycle::State&);
+  on_error(const rclcpp_lifecycle::State &);
 
 private:
-
-  bool changeState(const std::string& node_name, std::uint8_t transition);
+  bool changeState(const std::string & node_name, std::uint8_t transition);
   bool changeRobotCommandingState(bool is_active);
   void robotCommandingStateChanged(bool is_active);
-  std::vector<rclcpp::Subscription<lifecycle_msgs::msg::Transition>::SharedPtr> lifecycle_subscriptions_;
+  std::vector<rclcpp::Subscription<lifecycle_msgs::msg::Transition>::SharedPtr>
+  lifecycle_subscriptions_;
   rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr robot_commanding_state_subscription_;
   rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr change_robot_commanding_state_client_;
   bool robot_control_active_;
@@ -58,11 +69,11 @@ private:
   rclcpp::callback_group::CallbackGroup::SharedPtr cbg_;
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn SUCCESS =
-      rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn ERROR =
-      rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn FAILURE =
-      rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::FAILURE;
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::FAILURE;
 
   const std::string JOINT_CONTROLLER = "joint_controller";
   const std::string CONTROL_LOGIC = "keyboard_control";
@@ -70,9 +81,6 @@ private:
 };
 
 
+}  // namespace teleop_guided_robot
 
-
-}
-
-#endif /* INCLUDE_CONTROL_SYSTEM_KEYBOARD_CONTROL_HPP_ */
-
+#endif  // CONTROL_SYSTEM__SYSTEM_MANAGER_HPP_
