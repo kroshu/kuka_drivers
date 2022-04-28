4b4da37abd0d0fa04e9fa15099906c3285979592
Aron Svastits <svastits.aron@gmail.com>
2022-04-28 11:15:43 +0200

    fix include file name

--- kuka_sunrise_control/robot_control/src/robot_control/joint_controller_base.cpp
+++ kuka_sunrise_control/robot_control/src/robot_control/joint_controller_base.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "robot_control/joint_controller.hpp"
+#include "robot_control/joint_controller_base.hpp"
 
 #include <sys/mman.h>
 #include <string>
