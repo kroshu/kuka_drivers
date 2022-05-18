1d799b819f4c4d2175daa7e91259a34ced0a70ce
Aron Svastits <svastits.aron@gmail.com>
2022-05-18 09:55:14 +0200

    merge with kinect system_manager

--- kuka_sunrise_control/teleop_guided_robot/include/control_system/system_manager.hpp
+++ kuka_sunrise_control/teleop_guided_robot/include/control_system/system_manager.hpp
@@ -12,12 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 #ifndef CONTROL_SYSTEM__SYSTEM_MANAGER_HPP_
 #define CONTROL_SYSTEM__SYSTEM_MANAGER_HPP_
 
-#include <string>
 #include <vector>
+#include <memory>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
@@ -25,10 +25,15 @@
 #include "lifecycle_msgs/msg/state.hpp"
 #include "std_msgs/msg/bool.hpp"
 #include "std_srvs/srv/set_bool.hpp"
+#include "std_srvs/srv/trigger.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+#include "kuka_sunrise_interfaces/srv/get_state.hpp"
+#include "kuka_sunrise/internal/service_tools.hpp"
 
-namespace teleop_guided_robot
+namespace control_system
 {
 
+// TODO(kovacsge11) maybe derive from ROS2BaseNode
 class SystemManager : public rclcpp_lifecycle::LifecycleNode
 {
 public:
@@ -38,35 +43,41 @@ public:
   void deactivateControl();
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_configure(const rclcpp_lifecycle::State &);
-
+  on_configure(const rclcpp_lifecycle::State &) final;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_cleanup(const rclcpp_lifecycle::State &);
-
+  on_cleanup(const rclcpp_lifecycle::State &) final;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_shutdown(const rclcpp_lifecycle::State &);
-
+  on_shutdown(const rclcpp_lifecycle::State &) final;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_activate(const rclcpp_lifecycle::State &);
-
+  on_activate(const rclcpp_lifecycle::State &) final;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_deactivate(const rclcpp_lifecycle::State &);
-
+  on_deactivate(const rclcpp_lifecycle::State &) final;
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-  on_error(const rclcpp_lifecycle::State &);
+  on_error(const rclcpp_lifecycle::State &) final;
 
 private:
   bool changeState(const std::string & node_name, std::uint8_t transition);
   bool changeRobotCommandingState(bool is_active);
   void robotCommandingStateChanged(bool is_active);
-  std::vector<rclcpp::Subscription<lifecycle_msgs::msg::Transition>::SharedPtr>
-  lifecycle_subscriptions_;
-  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr robot_commanding_state_subscription_;
-  rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr change_robot_commanding_state_client_;
-  bool robot_control_active_;
+  void getFRIState();
+  void monitoringLoop();
+  bool robot_control_active_ = false;
+  bool stop_ = false;
+  int lbr_state_ = 0;
+  const std::chrono::milliseconds sleeping_time_ms_ = std::chrono::milliseconds(
+    200);
 
-  rclcpp::QoS qos_;
-  rclcpp::callback_group::CallbackGroup::SharedPtr cbg_;
+  std::thread polling_thread_;
+
+  rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::JointState>::SharedPtr
+    reference_joint_state_publisher_;
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr robot_commanding_state_subscription_;
+  rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Bool>::SharedPtr manage_processing_publisher_;
+  rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr change_robot_manager_state_client_;
+  rclcpp::Client<kuka_sunrise_interfaces::srv::GetState>::SharedPtr get_state_client_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr trigger_change_service_;
+  rclcpp::CallbackGroup::SharedPtr cbg_;
+  rclcpp::QoS qos_ = rclcpp::QoS(rclcpp::KeepLast(10));
 
   rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn SUCCESS =
     rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
@@ -79,8 +90,6 @@ private:
   const std::string CONTROL_LOGIC = "keyboard_control";
   const std::string ROBOT_INTERFACE = "robot_manager";
 };
-
-
-}  // namespace teleop_guided_robot
+}  // namespace control_system
 
 #endif  // CONTROL_SYSTEM__SYSTEM_MANAGER_HPP_
--- kuka_sunrise_control/teleop_guided_robot/src/control_system/system_manager.cpp
+++ kuka_sunrise_control/teleop_guided_robot/src/control_system/system_manager.cpp
@@ -14,107 +14,196 @@
 
 #include <string>
 #include <memory>
+#include <thread>
 
 #include "control_system/system_manager.hpp"
-#include "kuka_sunrise/internal/service_tools.hpp"
 
-namespace teleop_guided_robot
+namespace control_system
 {
 
-SystemManager::SystemManager(const std::string & node_name, const rclcpp::NodeOptions & options)
-: LifecycleNode(node_name, options), robot_control_active_(false), qos_(rclcpp::KeepLast(10))
+SystemManager::SystemManager(
+  const std::string & node_name,
+  const rclcpp::NodeOptions & options)
+: LifecycleNode(node_name, options)
 {
   qos_.reliable();
-  cbg_ = this->create_callback_group(rclcpp::callback_group::CallbackGroupType::MutuallyExclusive);
-  change_robot_commanding_state_client_ = this->create_client<std_srvs::srv::SetBool>(
-    ROBOT_INTERFACE + "/set_commanding_state", qos_.get_rmw_qos_profile(), cbg_);
-  robot_commanding_state_subscription_ = this->create_subscription<std_msgs::msg::Bool>(
-    ROBOT_INTERFACE + "/commanding_state_changed", qos_, [this](std_msgs::msg::Bool::SharedPtr msg)
-    {
+  cbg_ = this->create_callback_group(
+    rclcpp::CallbackGroupType::MutuallyExclusive);
+  change_robot_manager_state_client_ = this->create_client<
+    std_srvs::srv::SetBool>(
+    ROBOT_INTERFACE + "/set_commanding_state",
+    qos_.get_rmw_qos_profile(), cbg_);
+  robot_commanding_state_subscription_ = this->create_subscription<
+    std_msgs::msg::Bool>(
+    ROBOT_INTERFACE + "/commanding_state_changed", qos_,
+    [this](std_msgs::msg::Bool::SharedPtr msg) {
       this->robotCommandingStateChanged(msg->data);
     });
+
+  get_state_client_ =
+    this->create_client<kuka_sunrise_interfaces::srv::GetState>(
+    "robot_control/get_fri_state");
+  manage_processing_publisher_ = this->create_publisher<std_msgs::msg::Bool>(
+    "system_manager/manage", 1);
+  manage_processing_publisher_->on_activate();
+  auto trigger_change_callback = [this](
+    std_srvs::srv::Trigger::Request::SharedPtr,
+    std_srvs::srv::Trigger::Response::SharedPtr response) {
+      response->success = true;
+
+      if (this->get_current_state().label() == "active") {
+        std_msgs::msg::Bool activate;
+        activate.data = false;
+        manage_processing_publisher_->publish(activate);
+        if (this->deactivate().label() != "inactive") {
+          response->success = false;
+        }
+        RCLCPP_WARN(
+          get_logger(),
+          "Motion stopped externally, deactivating controls and managers");
+      } else {
+        RCLCPP_WARN(
+          get_logger(),
+          "Invalid request, system manager not active");
+      }
+    };
+  trigger_change_service_ = this->create_service<std_srvs::srv::Trigger>(
+    "system_manager/trigger_change", trigger_change_callback);
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn SystemManager::
 on_configure(
-  const rclcpp_lifecycle::State & state)
+  const rclcpp_lifecycle::State &)
 {
-  (void)state;
-  if (!changeState(ROBOT_INTERFACE, lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE)) {
+  if (!changeState(
+      ROBOT_INTERFACE,
+      lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE))
+  {
     return FAILURE;
   }
-  if (!changeState(JOINT_CONTROLLER, lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE)) {
+  if (!changeState(
+      JOINT_CONTROLLER,
+      lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE))
+  {
+    if (!changeState(
+        ROBOT_INTERFACE,
+        lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP))
+    {
+      RCLCPP_ERROR(get_logger(), "Could not solve differing states, restart needed");
+    }
     return ERROR;
   }
+
   if (!changeState(CONTROL_LOGIC, lifecycle_msgs::msg::Transition::TRANSITION_CONFIGURE)) {
-    return ERROR;
+    return FAILURE;
   }
   return SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn SystemManager::on_cleanup(
-  const rclcpp_lifecycle::State & state)
+  const rclcpp_lifecycle::State &)
 {
-  (void)state;
-  if (!changeState(ROBOT_INTERFACE, lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP)) {
+  if (!changeState(
+      ROBOT_INTERFACE,
+      lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP))
+  {
+    return FAILURE;
+  }
+
+  if (!changeState(
+      JOINT_CONTROLLER,
+      lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP))
+  {
     return FAILURE;
   }
   if (!changeState(CONTROL_LOGIC, lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP)) {
-    return ERROR;
+    return FAILURE;
   }
-  if (!changeState(JOINT_CONTROLLER, lifecycle_msgs::msg::Transition::TRANSITION_CLEANUP)) {
-    return ERROR;
-  }
+
   return SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-SystemManager::on_activate(
-  const rclcpp_lifecycle::State & state)
+SystemManager::on_activate(const rclcpp_lifecycle::State &)
 {
-  (void)state;
-  if (!changeState(ROBOT_INTERFACE, lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE)) {
+  if (!changeState(
+      ROBOT_INTERFACE,
+      lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE))
+  {
     return FAILURE;
   }
-  if (!changeState(JOINT_CONTROLLER, lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE)) {
-    return ERROR;
+  if (!changeState(
+      JOINT_CONTROLLER,
+      lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE))
+  {
+    if (!changeState(
+        ROBOT_INTERFACE,
+        lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE))
+    {
+      RCLCPP_ERROR(get_logger(), "Could not solve differing states, restart needed");
+      return ERROR;
+    }
+    return FAILURE;
   }
   if (!robot_control_active_ && !changeRobotCommandingState(true)) {
-    return ERROR;
+    if (!changeState(
+        ROBOT_INTERFACE,
+        lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE) ||
+      !changeState(
+        JOINT_CONTROLLER,
+        lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE))
+    {
+      RCLCPP_ERROR(get_logger(), "Could not solve differing states, restart needed");
+      return ERROR;
+    }
+    return FAILURE;
   }
-  robot_control_active_ = true;
   if (!changeState(CONTROL_LOGIC, lifecycle_msgs::msg::Transition::TRANSITION_ACTIVATE)) {
-    return ERROR;
+    return FAILURE;
   }
+
+  polling_thread_ = std::thread(&SystemManager::monitoringLoop, this);
+  robot_control_active_ = true;
+  std_msgs::msg::Bool activate;
+  activate.data = true;
+  manage_processing_publisher_->publish(activate);
   return SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn SystemManager::
 on_deactivate(
-  const rclcpp_lifecycle::State & state)
+  const rclcpp_lifecycle::State &)
 {
-  (void)state;
+  if (robot_control_active_ && !changeRobotCommandingState(false)) {
+    return FAILURE;
+  }
+  robot_control_active_ = false;
+  if (polling_thread_.joinable()) {polling_thread_.join();}
+  if (!changeState(
+      ROBOT_INTERFACE,
+      lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE))
+  {
+    return FAILURE;
+  }
+  if (!changeState(
+      JOINT_CONTROLLER,
+      lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE))
+  {
+    return FAILURE;
+  }
+
   if (!changeState(CONTROL_LOGIC, lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE)) {
     return FAILURE;
   }
-  if (robot_control_active_ && !changeRobotCommandingState(false)) {
-    return ERROR;
-  }
-  robot_control_active_ = false;
-  if (!changeState(ROBOT_INTERFACE, lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE)) {
-    return ERROR;
-  }
-  if (!changeState(JOINT_CONTROLLER, lifecycle_msgs::msg::Transition::TRANSITION_DEACTIVATE)) {
-    return ERROR;
-  }
+
   return SUCCESS;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
-SystemManager::on_shutdown(
-  const rclcpp_lifecycle::State & state)
+SystemManager::on_shutdown(const rclcpp_lifecycle::State & state)
 {
-  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn result = SUCCESS;
+  rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn result =
+    SUCCESS;
   switch (state.id()) {
     case lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE:
       result = this->on_deactivate(get_current_state());
@@ -132,39 +221,51 @@ SystemManager::on_shutdown(
       break;
   }
   if (!changeState(
-      CONTROL_LOGIC,
+      ROBOT_INTERFACE,
       lifecycle_msgs::msg::Transition::TRANSITION_UNCONFIGURED_SHUTDOWN))
   {
     return FAILURE;
   }
   if (!changeState(
-      ROBOT_INTERFACE,
-      lifecycle_msgs::msg::Transition::TRANSITION_UNCONFIGURED_SHUTDOWN))
-  {
-    return ERROR;
-  }
-  if (!changeState(
       JOINT_CONTROLLER,
       lifecycle_msgs::msg::Transition::TRANSITION_UNCONFIGURED_SHUTDOWN))
   {
-    return ERROR;
+    return FAILURE;
   }
+
+  if (!changeState(
+      CONTROL_LOGIC,
+      lifecycle_msgs::msg::Transition::TRANSITION_UNCONFIGURED_SHUTDOWN))
+  {
+    return FAILURE;
+  }
+
+
   return result;
 }
 
 rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn SystemManager::on_error(
-  const rclcpp_lifecycle::State & state)
+  const rclcpp_lifecycle::State &)
 {
-  (void)state;
   RCLCPP_INFO(get_logger(), "An error occured");
   return SUCCESS;
 }
 
-bool SystemManager::changeState(const std::string & node_name, std::uint8_t transition)
+void SystemManager::monitoringLoop()
+{
+  while (this->get_current_state().label() == "active") {
+    getFRIState();
+    std::this_thread::sleep_for(sleeping_time_ms_);
+  }
+  RCLCPP_WARN(get_logger(), "Stopping monitoring loop");
+}
+
+bool SystemManager::changeState(
+  const std::string & node_name,
+  std::uint8_t transition)
 {
   auto client = this->create_client<lifecycle_msgs::srv::ChangeState>(
-    node_name + "/change_state",
-    qos_.get_rmw_qos_profile(), cbg_);
+    node_name + "/change_state", qos_.get_rmw_qos_profile(), cbg_);
   auto request = std::make_shared<lifecycle_msgs::srv::ChangeState::Request>();
   request->transition.id = transition;
   if (!client->wait_for_service(std::chrono::milliseconds(2000))) {
@@ -172,40 +273,86 @@ bool SystemManager::changeState(const std::string & node_name, std::uint8_t tran
     return false;
   }
   auto future_result = client->async_send_request(request);
-  auto future_status =
-    kuka_sunrise::wait_for_result(future_result, std::chrono::milliseconds(3000));
+  auto future_status = kuka_sunrise::wait_for_result(
+    future_result,
+    std::chrono::milliseconds(3000));
   if (future_status != std::future_status::ready) {
-    RCLCPP_ERROR(get_logger(), "Future status not ready");
+    RCLCPP_ERROR(get_logger(), "Future status not ready, could not change state of " + node_name);
     return false;
   }
   if (future_result.get()->success) {
     return true;
   } else {
-    RCLCPP_ERROR(get_logger(), "Future result not success");
+    RCLCPP_ERROR(get_logger(), "Future result not success, could not change state of " + node_name);
     return false;
   }
 }
 
+void SystemManager::getFRIState()
+{
+  while (!get_state_client_->wait_for_service(std::chrono::milliseconds(1000))) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(
+        this->get_logger(),
+        "Interrupted while waiting for the service. Exiting.");
+      return;
+    }
+    RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
+  }
+  auto request = std::make_shared<
+    kuka_sunrise_interfaces::srv::GetState::Request>();
+
+  auto response_received_callback =
+    [this](
+    rclcpp::Client<kuka_sunrise_interfaces::srv::GetState>::SharedFuture future) {
+      auto result = future.get();
+      lbr_state_ = static_cast<int>(result->data);
+
+      // Two consecutive non-four states are needed for shutdown
+      // to avoid unnecessary stops
+      if (lbr_state_ != 4 && !stop_) {
+        stop_ = true;
+      } else if (lbr_state_ != 4 && stop_) {
+        std_msgs::msg::Bool activate;
+        activate.data = false;
+        manage_processing_publisher_->publish(activate);
+      } else {
+        stop_ = false;
+      }
+      RCLCPP_DEBUG(this->get_logger(), "State: %i", lbr_state_);
+    };
+  auto future_result = get_state_client_->async_send_request(
+    request,
+    response_received_callback);
+}
+
+// Activate the ActivatableInterface of robot_manager_node
 bool SystemManager::changeRobotCommandingState(bool is_active)
 {
   auto request = std::make_shared<std_srvs::srv::SetBool::Request>();
   request->data = is_active;
-  if (!change_robot_commanding_state_client_->wait_for_service(std::chrono::milliseconds(2000))) {
+  if (!change_robot_manager_state_client_->wait_for_service(
+      std::chrono::milliseconds(2000)))
+  {
     RCLCPP_ERROR(get_logger(), "Wait for service failed");
     return false;
   }
-  auto future_result = change_robot_commanding_state_client_->async_send_request(request);
-  auto future_status =
-    kuka_sunrise::wait_for_result(future_result, std::chrono::milliseconds(3000));
+  auto future_result =
+    change_robot_manager_state_client_->async_send_request(request);
+  auto future_status = kuka_sunrise::wait_for_result(
+    future_result,
+    std::chrono::milliseconds(3000));
   if (future_status != std::future_status::ready) {
-    RCLCPP_ERROR(get_logger(), "Future status not ready");
+    RCLCPP_ERROR(get_logger(), "Future status not ready, could not change robot commanding state");
     return false;
   }
   if (future_result.get()->success) {
     robot_control_active_ = true;
     return true;
   } else {
-    RCLCPP_ERROR(get_logger(), "Future result not success");
+    RCLCPP_ERROR(
+      get_logger(),
+      "Future result not success, could not change robot commanding state");
     return false;
   }
 }
@@ -214,22 +361,21 @@ void SystemManager::robotCommandingStateChanged(bool is_active)
 {
   if (is_active == false && this->get_current_state().label() == "active") {
     robot_control_active_ = false;
-    // TODO(resizoltan) check if successful
+    // TODO(resizoltan): check if successful
     this->deactivate();
   }
 }
 
-}  // namespace teleop_guided_robot
+}  // namespace control_system
 
 int main(int argc, char * argv[])
 {
-  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+  setvbuf(stdout, nullptr, _IONBF, BUFSIZ);
 
   rclcpp::init(argc, argv);
   rclcpp::executors::MultiThreadedExecutor executor;
-  auto node = std::make_shared<teleop_guided_robot::SystemManager>(
-    "system_manager",
-    rclcpp::NodeOptions());
+  auto node = std::make_shared<control_system::SystemManager>(
+    "system_manager", rclcpp::NodeOptions());
   executor.add_node(node->get_node_base_interface());
   executor.spin();
   rclcpp::shutdown();
