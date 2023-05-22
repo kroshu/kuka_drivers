bdd1003ebb7ce38b5c0ee545af32822dfdffeb92
Svastits <svastits1@gmail.com>
2023-05-22 14:22:39 +0200

    Revert "Merge branch 'master' of https://github.com/kroshu/ros2_kuka_drivers"
    This reverts commit e7afe719b581b3552960ce090fc917a2936fb609, reversing
    changes made to 86784b0b319ead3d2d329197397e371a94121c1a.

--- kuka_driver_examples/eci_demo/src/MoveitBasicPlan.cpp
+++ kuka_driver_examples/eci_demo/src/MoveitBasicPlan.cpp
@@ -35,9 +35,6 @@ int main(int argc, char * argv[])
     "moveit_circle",
     rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
   );
-  rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(node);
-  std::thread([&executor]() {executor.spin();}).detach();
 
   // Create a ROS logger
   auto const logger = rclcpp::get_logger("moveit_basic_plan");
@@ -110,9 +107,6 @@ int main(int argc, char * argv[])
       waypoints.push_back(msg);
     }
   }
-
-  // Get the current joint values
-  auto jv = move_group_interface.getCurrentJointValues();
 
   RCLCPP_INFO(logger, "Start planning");
   // Planning
