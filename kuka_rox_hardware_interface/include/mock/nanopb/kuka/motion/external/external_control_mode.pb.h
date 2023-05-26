97cd1a65f3d24cc039f38e7af0494b6f8a92b7f9
Kristofi Misi <krmihaly42@gmail.com>
2023-05-26 17:44:39 +0200

    Add mobile velocity control to control mode mock

--- kuka_rox_hardware_interface/include/mock/nanopb/kuka/motion/external/external_control_mode.pb.h
+++ kuka_rox_hardware_interface/include/mock/nanopb/kuka/motion/external/external_control_mode.pb.h
@@ -6,19 +6,25 @@
 #error Regenerate this file with the current version of nanopb generator.
 #endif
 
-typedef enum _kuka_motion_external_ExternalControlMode { 
-    kuka_motion_external_ExternalControlMode_EXTERNAL_CONTROL_MODE_UNSPECIFIED = 0, 
-    kuka_motion_external_ExternalControlMode_POSITION_CONTROL = 1, 
-    kuka_motion_external_ExternalControlMode_CARTESIAN_OVERLAY = 2, 
-    kuka_motion_external_ExternalControlMode_JOINT_IMPEDANCE_CONTROL = 3, 
-    kuka_motion_external_ExternalControlMode_CARTESIAN_IMPEDANCE_CONTROL = 4, 
-    kuka_motion_external_ExternalControlMode_TORQUE_CONTROL = 5 
+typedef enum _kuka_motion_external_ExternalControlMode
+{
+  kuka_motion_external_ExternalControlMode_EXTERNAL_CONTROL_MODE_UNSPECIFIED = 0,
+  kuka_motion_external_ExternalControlMode_POSITION_CONTROL = 1,
+  kuka_motion_external_ExternalControlMode_CARTESIAN_OVERLAY = 2,
+  kuka_motion_external_ExternalControlMode_JOINT_IMPEDANCE_CONTROL = 3,
+  kuka_motion_external_ExternalControlMode_CARTESIAN_IMPEDANCE_CONTROL = 4,
+  kuka_motion_external_ExternalControlMode_TORQUE_CONTROL = 5,
+  kuka_motion_external_ExternalControlMode_MR_VELOCITY_CONTROL = 6
 } kuka_motion_external_ExternalControlMode;
 
 /* Helper constants for enums */
-#define _kuka_motion_external_ExternalControlMode_MIN kuka_motion_external_ExternalControlMode_EXTERNAL_CONTROL_MODE_UNSPECIFIED
-#define _kuka_motion_external_ExternalControlMode_MAX kuka_motion_external_ExternalControlMode_TORQUE_CONTROL
-#define _kuka_motion_external_ExternalControlMode_ARRAYSIZE ((kuka_motion_external_ExternalControlMode)(kuka_motion_external_ExternalControlMode_TORQUE_CONTROL+1))
+#define _kuka_motion_external_ExternalControlMode_MIN \
+  kuka_motion_external_ExternalControlMode_EXTERNAL_CONTROL_MODE_UNSPECIFIED
+#define _kuka_motion_external_ExternalControlMode_MAX \
+  kuka_motion_external_ExternalControlMode_MR_VELOCITY_CONTROL
+#define _kuka_motion_external_ExternalControlMode_ARRAYSIZE (( \
+    kuka_motion_external_ExternalControlMode)( \
+    kuka_motion_external_ExternalControlMode_MR_VELOCITY_CONTROL + 1))
 
 
 #ifdef __cplusplus
