b59d55e08ef664b8621d1a81981e33be97053ab4
Aron Svastits <svastits1@gmail.com>
2023-08-21 17:08:54 +0200

    use gazebo_sim arg

--- kuka_rox_hardware_interface/launch/startup_gazebo.launch.py
+++ kuka_rox_hardware_interface/launch/startup_gazebo.launch.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
@@ -30,9 +28,6 @@ from launch.event_handlers import OnProcessExit
 
 
 def generate_launch_description():
-    # startup_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource(
-    #     [get_package_share_directory('kuka_rox_hw_interface'), '/launch/startup.launch.py']))
-
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
@@ -42,6 +37,8 @@ def generate_launch_description():
                  "urdf", "lbr_iisy3_r760.urdf.xacro"]
             ),
             " ",
+            "gazebo_sim:=",
+            "true",
         ]
     )
     robot_description = {'robot_description': robot_description_content}
