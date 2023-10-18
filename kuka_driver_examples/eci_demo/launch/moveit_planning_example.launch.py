ef436771d4190b6a0497c607b3e2a29125653f1b
Aron Svastits <svastits1@gmail.com>
2023-10-18 18:01:28 +0200

    update planning freq

--- kuka_driver_examples/eci_demo/launch/moveit_planning_example.launch.py
+++ kuka_driver_examples/eci_demo/launch/moveit_planning_example.launch.py
@@ -51,7 +51,7 @@ def launch_setup(context, *args, **kwargs):
         package="moveit_ros_move_group",
         executable="move_group",
         output="screen",
-        parameters=[moveit_config.to_dict()],
+        parameters=[moveit_config.to_dict(), {'publish_planning_scene_hz': 30.0}],
     )
 
     rviz = Node(
