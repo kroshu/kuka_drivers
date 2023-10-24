2f9c4e4e87c4648ddd85d421ee778ab0254882eb
Aron Svastits <svastits.aron@gmail.com>
2023-10-24 09:12:20 +0200

    fix scaling setting

--- kuka_driver_examples/eci_demo/src/MoveitBasicPlannersExample.cpp
+++ kuka_driver_examples/eci_demo/src/MoveitBasicPlannersExample.cpp
@@ -54,7 +54,8 @@ int main(int argc, char * argv[])
   }
 
   example_node->addBreakPoint();
-
+  example_node->moveGroupInterface()->setMaxVelocityScalingFactor(0.1);
+  example_node->moveGroupInterface()->setMaxAccelerationScalingFactor(0.1);
   // Pilz LIN planner
   auto cart_goal = Eigen::Isometry3d(
     Eigen::Translation3d(
@@ -76,8 +77,6 @@ int main(int argc, char * argv[])
   example_node->addBreakPoint();
 
   // Try moving back with Pilz LIN
-  example_node->moveGroupInterface()->setMaxVelocityScalingFactor(0.1);
-  example_node->moveGroupInterface()->setMaxAccelerationScalingFactor(0.1);
   planned_trajectory = example_node->planToPoint(
     standing_pose, "pilz_industrial_motion_planner",
     "LIN");
