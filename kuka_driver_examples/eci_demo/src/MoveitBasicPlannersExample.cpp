71287641c9cb52249bbcda69ed49eb07229dbe18
Aron Svastits <svastits.aron@gmail.com>
2023-10-25 15:24:19 +0200

    remove trigger

--- kuka_driver_examples/eci_demo/src/MoveitBasicPlannersExample.cpp
+++ kuka_driver_examples/eci_demo/src/MoveitBasicPlannersExample.cpp
@@ -102,7 +102,6 @@ int main(int argc, char * argv[])
   } else {
     example_node->drawTitle("Failed planning with Pilz PTP");
   }
-  example_node->addBreakPoint();
 
   // Shutdown ROS
   rclcpp::shutdown();
