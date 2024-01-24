fa538285976c2febc071d01fe2edcba051ddacc3
Aron Svastits <svastits1@gmail.com>
2024-01-24 08:20:17 +0100

    remove configuring from rights

--- kuka_drivers_core/include/kuka_drivers_core/parameter_handler.hpp
+++ kuka_drivers_core/include/kuka_drivers_core/parameter_handler.hpp
@@ -31,7 +31,6 @@ struct ParameterSetAccessRights
   bool unconfigured;
   bool inactive;
   bool active;
-  bool configuring = false;
   bool isSetAllowed(std::uint8_t current_state) const
   {
     switch (current_state)
@@ -42,8 +41,6 @@ struct ParameterSetAccessRights
         return inactive;
       case lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE:
         return active;
-      case lifecycle_msgs::msg::State::TRANSITION_STATE_CONFIGURING:
-        return configuring;
       default:
         return false;
     }
