b78026a04d06b43d04659cc3c2a921aaae9b837d
Aron Svastits <svastits1@gmail.com>
2024-02-23 16:21:29 +0100

    debug

--- .github/workflows/install_sdk.sh
+++
@@ -1,6 +0,0 @@
-cd /home/runner/work
-git clone https://github.com/kroshu/kuka-external-control-sdk.git
-mkdir -p /home/runner/work/kuka-external-control-sdk/kuka-external-control-sdk/build
-cd /home/runner/work/kuka-external-control-sdk/kuka-external-control-sdk/build
-cmake ..
-make install
