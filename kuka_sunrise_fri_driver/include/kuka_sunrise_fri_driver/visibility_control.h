5179fc6522fac5564ca7322c2c5367ef4a44cb5e
Aron Svastits <svastits1@gmail.com>
2024-04-10 15:08:48 +0200

    license name fix

--- kuka_sunrise_fri_driver/include/kuka_sunrise_fri_driver/visibility_control.h
+++ kuka_sunrise_fri_driver/include/kuka_sunrise_fri_driver/visibility_control.h
@@ -1,4 +1,4 @@
-//    Copyright 2023 Áron Svastits
+//    Copyright 2023 Aron Svastits
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
