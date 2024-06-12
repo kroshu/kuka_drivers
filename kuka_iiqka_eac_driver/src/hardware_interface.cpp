997a7ff393560284f2785d82eba21d9e286c91e6
Kristófi Mihály <krmihaly42@gmail.com>
2024-06-12 13:25:15 +0200

    adapt to sdk changes

--- kuka_iiqka_eac_driver/src/hardware_interface.cpp
+++ kuka_iiqka_eac_driver/src/hardware_interface.cpp
@@ -176,10 +176,10 @@ return_type KukaEACHardwareInterface::read(const rclcpp::Time &, const rclcpp::D
     auto & req_message = robot_ptr_->GetLastMotionState();
 
     std::copy(
-      req_message.GetMeasuredPositions()->begin(), req_message.GetMeasuredPositions()->end(),
+      req_message.GetMeasuredPositions().begin(), req_message.GetMeasuredPositions().end(),
       hw_position_states_.begin());
     std::copy(
-      req_message.GetMeasuredTorques()->begin(), req_message.GetMeasuredTorques()->end(),
+      req_message.GetMeasuredTorques().begin(), req_message.GetMeasuredTorques().end(),
       hw_torque_states_.begin());
 
     if (cycle_count_ == 0)
@@ -206,11 +206,11 @@ return_type KukaEACHardwareInterface::write(const rclcpp::Time &, const rclcpp::
   }
 
   robot_ptr_->GetControlSignal().AddJointPositionValues(
-    hw_position_commands_);
+    hw_position_commands_.begin(),hw_position_commands_.end());
   robot_ptr_->GetControlSignal().AddTorqueValues(
-    hw_torque_commands_);
+    hw_torque_commands_.begin(), hw_torque_commands_.end());
   robot_ptr_->GetControlSignal().AddStiffnessAndDampingValues(
-    hw_stiffness_commands_, hw_damping_commands_);
+    hw_stiffness_commands_.begin(), hw_stiffness_commands_.end(), hw_damping_commands_.begin(), hw_damping_commands_.end());
 
   kuka::external::control::Status send_reply;
   if (stop_requested_)
