40b7432281f61da389f689fb3085dbb83199bfd5
root <root@PC22846.kuka.int.kuka.com>
2024-12-19 15:14:05 +0100

    Revert "fix?"
    This reverts commit 4284a009d76f5bf5197f20850d4f3d425f6a29ec.

--- kuka_iiqka_eac_driver/launch/startup_with_rviz.launch.py
+++ kuka_iiqka_eac_driver/launch/startup_with_rviz.launch.py
@@ -35,12 +35,6 @@ def generate_launch_description():
         ),
     )
 
-    robot_description_kinematics = {
-        "robot_description_kinematics": {
-            "manipulator": {"kinematics_solver": "kdl_kinematics_plugin/KDLKinematicsPlugin"}
-        }
-    }
-
     startup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [get_package_share_directory("kuka_iiqka_eac_driver"), "/launch/startup.launch.py"]
@@ -57,9 +51,6 @@ def generate_launch_description():
                 name="rviz2",
                 output="log",
                 arguments=["-d", rviz_config, "--ros-args", "--log-level", "error"],
-                parameters=[
-                    robot_description_kinematics,
-                ],
             ),
         ]
     )
--- kuka_sunrise_fri_driver/launch/startup_with_rviz.launch.py
+++ kuka_sunrise_fri_driver/launch/startup_with_rviz.launch.py
@@ -36,12 +36,6 @@ def generate_launch_description():
         ),
     )
 
-    robot_description_kinematics = {
-        "robot_description_kinematics": {
-            "manipulator": {"kinematics_solver": "kdl_kinematics_plugin/KDLKinematicsPlugin"}
-        }
-    }
-
     startup_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [get_package_share_directory("kuka_sunrise_fri_driver"), "/launch/startup.launch.py"]
@@ -58,9 +52,6 @@ def generate_launch_description():
                 name="rviz2",
                 output="log",
                 arguments=["-d", rviz_config, "--ros-args", "--log-level", "error"],
-                parameters=[
-                    robot_description_kinematics,
-                ],
             ),
         ]
     )
