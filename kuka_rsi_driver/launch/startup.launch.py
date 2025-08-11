6db5b47c9eab19cdafd470b7496aa81154c1dab5
Sándor Komáromi <sanyi.komaromi@gmail.com>
2025-08-11 15:21:06 +0200

    fix pre-commit format

--- kuka_rsi_driver/launch/startup.launch.py
+++ kuka_rsi_driver/launch/startup.launch.py
@@ -182,8 +182,7 @@ def launch_setup(context, *args, **kwargs):
 
         return Node(package="controller_manager", executable="spawner", arguments=arg_list)
 
-    controllers = {
-        "joint_state_broadcaster": None, "joint_trajectory_controller": jtc_config}
+    controllers = {"joint_state_broadcaster": None, "joint_trajectory_controller": jtc_config}
 
     if use_gpio.perform(context) == "true":
         controllers["gpio_controller"] = gpio_config
