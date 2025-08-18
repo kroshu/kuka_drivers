caaba0bf48448767a10b15c0965cfab394cc8387
Sándor Komáromi <sanyi.komaromi@gmail.com>
2025-08-18 11:57:14 +0200

    fix warnings

--- examples/iiqka_moveit_example/include/iiqka_moveit_example/moveit_example.hpp
+++ examples/iiqka_moveit_example/include/iiqka_moveit_example/moveit_example.hpp
@@ -22,8 +22,8 @@
 #include <vector>
 
 #include "geometry_msgs/msg/vector3.hpp"
-#include "moveit/move_group_interface/move_group_interface.h"
-#include "moveit/planning_scene_interface/planning_scene_interface.h"
+#include "moveit/move_group_interface/move_group_interface.hpp"
+#include "moveit/planning_scene_interface/planning_scene_interface.hpp"
 #include "moveit_msgs/msg/collision_object.hpp"
 #include "moveit_visual_tools/moveit_visual_tools.h"
 #include "rclcpp/rclcpp.hpp"
@@ -75,7 +75,7 @@ public:
 
     RCLCPP_INFO(LOGGER, "Start planning");
     double fraction =
-      move_group_interface_->computeCartesianPath(waypoints, 0.005, 0.0, trajectory);
+      move_group_interface_->computeCartesianPath(waypoints, 0.005, trajectory);
     RCLCPP_INFO(LOGGER, "Planning done!");
 
     if (fraction < 1)
