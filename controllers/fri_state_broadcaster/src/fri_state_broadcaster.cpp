392172fa70c775e0aac351bae682ef42713aefaa
Komáromi Sándor <sanyi.komaromi@gmail.com>
2025-08-25 16:57:26 +0200

    Solve warning on master for release (#246)
    * fix warnings on fri_state_broadcaster
    
    * fix warnings on kuka_event_broadcaster
    
    * add atomic for control_mode_handler
    
    * fix warning on control_mode_handler
    
    * remove rmw_qos_profile
    
    * fix warnings
    
    * fix joint_group_impedance_controller
    
    * fix cmake_c_standard warnings
    
    * fix format
    
    * fix format in joint inpedance controller
    
    * fix format
    
    * fix format #4
    
    * fix coments
    
    ---------
    
    Co-authored-by: Áron Svastits <49677296+Svastits@users.noreply.github.com>

--- controllers/fri_state_broadcaster/src/fri_state_broadcaster.cpp
+++ controllers/fri_state_broadcaster/src/fri_state_broadcaster.cpp
@@ -81,15 +81,70 @@ controller_interface::CallbackReturn FRIStateBroadcaster::on_deactivate(
 controller_interface::return_type FRIStateBroadcaster::update(
   const rclcpp::Time &, const rclcpp::Duration &)
 {
-  state_msg_.session_state = static_cast<int>(state_interfaces_[0].get_value());
-  state_msg_.connection_quality = static_cast<int>(state_interfaces_[1].get_value());
-  state_msg_.safety_state = static_cast<int>(state_interfaces_[2].get_value());
-  state_msg_.command_mode = static_cast<int>(state_interfaces_[3].get_value());
-  state_msg_.control_mode = static_cast<int>(state_interfaces_[4].get_value());
-  state_msg_.operation_mode = static_cast<int>(state_interfaces_[5].get_value());
-  state_msg_.drive_state = static_cast<int>(state_interfaces_[6].get_value());
-  state_msg_.overlay_type = static_cast<int>(state_interfaces_[7].get_value());
-  state_msg_.tracking_performance = state_interfaces_[8].get_value();
+  auto session_state =
+    static_cast<int32_t>(state_interfaces_[0].get_optional().value_or(state_msg_.session_state));
+  if (session_state != state_msg_.session_state)
+  {
+    state_msg_.session_state = session_state;  // Update session state if it has changed
+  }
+
+  auto connection_quality = static_cast<int32_t>(
+    state_interfaces_[1].get_optional().value_or(state_msg_.connection_quality));
+  if (connection_quality != state_msg_.connection_quality)
+  {
+    state_msg_.connection_quality =
+      connection_quality;  // Update connection quality if it has changed
+  }
+
+  auto safety_state =
+    static_cast<int32_t>(state_interfaces_[2].get_optional().value_or(state_msg_.safety_state));
+  if (safety_state != state_msg_.safety_state)
+  {
+    state_msg_.safety_state = safety_state;  // Update safety state if it has changed
+  }
+
+  auto command_mode =
+    static_cast<int32_t>(state_interfaces_[3].get_optional().value_or(state_msg_.command_mode));
+  if (command_mode != state_msg_.command_mode)
+  {
+    state_msg_.command_mode = command_mode;  // Update command mode if it has changed
+  }
+
+  auto control_mode =
+    static_cast<int32_t>(state_interfaces_[4].get_optional().value_or(state_msg_.control_mode));
+  if (control_mode != state_msg_.control_mode)
+  {
+    state_msg_.control_mode = control_mode;  // Update control mode state if it has changed
+  }
+
+  auto operation_mode =
+    static_cast<int32_t>(state_interfaces_[5].get_optional().value_or(state_msg_.operation_mode));
+  if (operation_mode != state_msg_.operation_mode)
+  {
+    state_msg_.operation_mode = operation_mode;  // Update operation mode if it has changed
+  }
+
+  auto drive_state =
+    static_cast<int32_t>(state_interfaces_[6].get_optional().value_or(state_msg_.drive_state));
+  if (drive_state != state_msg_.drive_state)
+  {
+    state_msg_.drive_state = drive_state;  // Update drive state if it has changed
+  }
+
+  auto overlay_type =
+    static_cast<int32_t>(state_interfaces_[7].get_optional().value_or(state_msg_.overlay_type));
+  if (overlay_type != state_msg_.overlay_type)
+  {
+    state_msg_.overlay_type = overlay_type;  // Update overlay type if it has changed
+  }
+
+  auto tracking_performance = static_cast<int32_t>(
+    state_interfaces_[8].get_optional().value_or(state_msg_.tracking_performance));
+  if (tracking_performance != state_msg_.tracking_performance)
+  {
+    state_msg_.tracking_performance =
+      tracking_performance;  // Update tracking performance if it has changed
+  }
 
   if (counter_++ == 10)
   {
