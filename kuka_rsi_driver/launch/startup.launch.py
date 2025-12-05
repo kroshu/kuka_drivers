df2364028d9f5439f55c0e2e316340804f9aa1c8
Svastits <49677296+Svastits@users.noreply.github.com>
2025-12-05 15:24:11 +0100

    merge fix

--- kuka_rsi_driver/launch/startup.launch.py
+++ kuka_rsi_driver/launch/startup.launch.py
@@ -173,18 +173,14 @@ def launch_setup(context, *args, **kwargs):
             arg_list.append("--inactive")
         return Node(package="controller_manager", executable="spawner", arguments=arg_list)
 
-    controllers = {
-        "joint_state_broadcaster": None,
-        "joint_trajectory_controller": jtc_config,
-        "event_broadcaster": None,
-    }
+    controller_names = {"joint_state_broadcaster", "joint_trajectory_controller", "event_broadcaster"}
 
     if use_gpio.perform(context) == "true":
         controller_names.append("gpio_controller")
 
     if driver_version.perform(context) == "eki_rsi":
-        controllers["control_mode_handler"] = None
-        controllers["kss_message_handler"] = None
+        controller_names.append("control_mode_handler")
+        controller_names.append("kss_message_handler")
 
     controller_spawners = [controller_spawner(name) for name in controller_names]
 
