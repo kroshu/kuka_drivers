218ddf3378e167b381d5e5b142e0f5e07dcf7e25
Svastits <49677296+Svastits@users.noreply.github.com>
2025-12-15 21:04:36 +0100

    copyright

--- kuka_iiqka_eac_driver/include/kuka_iiqka_eac_driver/visibility_control.h
+++ kuka_iiqka_eac_driver/include/kuka_iiqka_eac_driver/visibility_control.h
@@ -1,4 +1,4 @@
-//    Copyright 2023 Aron Svastits
+//    // Copyright 2025 KUKA Hungaria Kft.
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
